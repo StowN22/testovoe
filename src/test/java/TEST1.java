8ed09cdaf88f33dbe398a0477ff421dd44c89363
Alexey <stownkerch12@gmail.com>
2024-06-26 21:05:31 +0300

    123

--- src/test/java/TEST1.java
+++ src/test/java/TEST1.java
@@ -1,6 +1,7 @@
 package test;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -24,10 +25,12 @@ public class TEST1 {
 
         driver.get("https://market.yandex.ru");
 
-        //3. В разделе Маркет выбрать "Смартфоны"
+        //3. В разделе "Маркет" кликнуть на кнопку "Каталог"
 
-        WebElement element = driver.findElement(By.id("hamburger"));
+        WebElement element = driver.findElement(By.className("_2bsQ5"));
         element.click();
+
+        //4. В разделе "Каталог" кликнуть на кнопку "Смартфоны"
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
@@ -43,12 +46,12 @@ public class TEST1 {
         WebElement smartfones2 = driver.findElement(By.className("_1tdYt CCS_Z"));
         smartfones2.click();
 
-        //4. Зайти во "Все фильтры"
+        //5. Кликнуть на кнопку "Все фильтры"
 
         WebElement filter = driver.findElement(By.className("_1_C0_ _1n-yE _16wT_ _1Dmnq"));
         filter.click();
 
-        //5. Задать параметр поиска "Цена" до 20000 рублей и "Диагональ экрана" от 3 дюймов
+        //6. Задать параметр поиска "Цена" до 20000 рублей и "Диагональ экрана" от 3 дюймов
 
         WebElement overprice = driver.findElement(By.className("_2xtC2"));
         overprice.sendKeys("20000");
@@ -59,7 +62,7 @@ public class TEST1 {
         WebElement diagonal2 = driver.findElement(By.className("_2xtC2"));
         diagonal2.sendKeys("3");
 
-        //6. Выбрать не менее 5 любых производителей, среди популярных
+        //7. Выбрать не менее 5 любых производителей, среди популярных
 
         WebElement blackview = driver.findElement(By.xpath("//*[@id=\"10380976\"]/div"));
         blackview.click();
@@ -76,24 +79,34 @@ public class TEST1 {
         WebElement realme = driver.findElement(By.xpath("//*[@id=\"16713696\"]/div"));
         realme.click();
 
-        //7. Кликнуть кнопку "Показать товары"
+        //8. Кликнуть на кнопку "Показать товары"
 
         WebElement apply = driver.findElement(By.className("_2qvOO _3qN-v _1Rc6L"));
         apply.click();
 
-        //8. Проверить, что элементов на странице 10
+        //9. Проверить, что элементов на странице 10
 
         wait.until(ExpectedConditions.numberOfElementsToBe(By.className("EQlfk Gqfzd"), 10));
 
-        //9. Запомнить первый элемент в списке
+        //10. Запомнить первый элемент в списке
 
-        
+        WebElement firstProduct = driver.findElement(By.xpath("//*[@id=\"838hfq5wruc\"]/div/div/div[2]/div[1]/div/a/h3"));
 
+        //11. Изменить Сортировку на другую (популярность или новизна)
 
+        WebElement rating = driver.findElement(By.xpath("//*[@id=\"/content/page/fancyPage/cms/4/SearchFilters-SearchFilters\"]/div/div/div/div/div[19]/div/fieldset/div/div/div/div[2]/label/span/span[1]/span"));
+        rating.click();
 
+        //12. Найти и кликнуть по имени запомненного объекта
 
+        WebElement poisk = driver.findElement(By.className("_3TbaT mini-suggest__input"));
+        poisk.sendKeys("firstProduct");
+        poisk.sendKeys(Keys.ENTER);
 
+        //13. Вывести цифровое значение его оценки
 
-
+        firstProduct.click();
+        WebElement stars = driver.findElement(By.className("EQlfk _38X5e"));
+        stars.click();
     }
 }
